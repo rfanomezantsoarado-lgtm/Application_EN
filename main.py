0a4e7415a41623bad13f5c8cf5c2978ab62b6fd2
rfanomezantsoarado-lgtm <rfanomezantsoarado@gmail.com>
2026-05-16 22:00:05 +0300

    New update

--- main.py
+++ main.py
@@ -1,34 +1,69 @@
-#Importation des bibliothèques utile
+# Importation des bibliothèques utiles
+
 from kivy.config import Config
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager
-from kivy.logger import Logger
-import sys
-import os
 
 # Taille téléphone Android
 Config.set('graphics', 'width', '360')
 Config.set('graphics', 'height', '640')
 Config.set('graphics', 'resizable', '0')
 
-#Importation des autres scripts
+import traceback
+import os
+import sys
+
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from kivy.logger import Logger
+
+# Importation des écrans
 from accueil import AccueilScreen
 from clients import ClientScreen
 from produits import ProduitScreen
 from commande import CommandeScreen
 from historique_commande import HistoriqueCommandeScreen
 
+
 class ENApp(App):
+
     def build(self):
-         sm = ScreenManager()
 
-         sm.add_widget(AccueilScreen(name="accueil"))
-         sm.add_widget(ClientScreen(name="clients"))
-         sm.add_widget(ProduitScreen(name="produits"))
-         sm.add_widget(CommandeScreen(name="commande"))
-         sm.add_widget(HistoriqueCommandeScreen(name="historique_commande"))
+        try:
 
-         return sm
+            Logger.info("ENAPP: Demarrage application")
+
+            sm = ScreenManager()
+
+            sm.add_widget(AccueilScreen(name="accueil"))
+            sm.add_widget(ClientScreen(name="clients"))
+            sm.add_widget(ProduitScreen(name="produits"))
+            sm.add_widget(CommandeScreen(name="commande"))
+            sm.add_widget(HistoriqueCommandeScreen(name="historique_commande"))
+
+            return sm
+
+        except Exception as e:
+
+            print("ERREUR BUILD APPLICATION")
+            print(str(e))
+
+            traceback.print_exc()
+
+            Logger.error(f"ENAPP: {str(e)}")
+
+            raise e
+
 
 if __name__ == "__main__":
-    ENApp().run()
+
+    try:
+
+        ENApp().run()
+
+    except Exception as e:
+
+        print("ERREUR LANCEMENT APPLICATION")
+        print(str(e))
+
+        traceback.print_exc()
+
+        Logger.error(f"ENAPP: Crash -> {str(e)}")

b1882929ce2b754c427052434addd6662771fbe9
rfanomezantsoarado-lgtm <rfanomezantsoarado@gmail.com>
2026-05-18 06:35:58 +0300

    Delete recipes/fpdf2 directory

--- recipes/fpdf2/__init__.py
+++
@@ -1,9 +0,0 @@
-# recipes/fpdf2/__init__.py
-from pythonforandroid.recipe import PythonRecipe
-
-class FPDF2Recipe(PythonRecipe):
-    version = '2.7.4'
-    url = 'https://github.com/py-pdf/fpdf2/archive/refs/tags/{version}.tar.gz'
-    depends = ['python3']
-    
-recipe = FPDF2Recipe()
